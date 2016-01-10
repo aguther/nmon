a69d4e76d4036c7f12078a4e60905e71d7f311aa
Andreas Guther <github@guther.net>
2016-01-10 14:17:15 +0100

    Improved robustness, renamed file

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# defines
+NAME=nmon
+
+# get current version
+FILENAME=$(basename "$(ls lmon*.c)")
+VERSION=${FILENAME/lmon/}
+VERSION=${VERSION/.c/}
+
+# pack sources
+tar -cvzf "${NAME}-${VERSION}.tar.gz" --exclude .git --exclude "*.tar.gz" --transform s/./"${NAME}-${VERSION}"/ ./*
+
+# run rpm build command
+rpmbuild -ta "${NAME}-${VERSION}.tar.gz"
